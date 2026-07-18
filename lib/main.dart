19125457236f13993816cfb85cd12449f8652db5
Antony Day <antonyjday@gmail.com>
2026-07-18 20:01:10 +0100

    Wire up Firebase project and Google Maps API key
    Links the app to the convoy-app-ajd Firebase project (Firestore, Anonymous
    auth), and adds a Maps SDK for Android key restricted to this app's debug
    signing cert. Verified sign-in and group creation against the live backend
    on an Android emulator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'services/auth_service.dart';
 import 'services/deep_link_service.dart';
 import 'screens/home_screen.dart';
@@ -7,7 +8,7 @@ import 'screens/sign_in_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await DeepLinkService.instance.init();
   runApp(const ConvoyApp());
 }
